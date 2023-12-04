9815de16c29fd2ae352197b30f842abf38bf7170
Dezedene <gregoire.suray@gmail.com>
2023-12-04 14:39:46 +0100

    initialized project + start page routing

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function Home() {
   return (
     <>
      <div>
-      <h1>Hello World</h1>
+      <h1>Home Page</h1>
      </div>
     </>
   )
 }
+
+export default Home

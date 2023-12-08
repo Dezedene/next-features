6c41f139936b2464ba40842a4ceb8549537f6ee2
Dezedene <gregoire.suray@gmail.com>
2023-12-08 16:10:07 +0100

    added new files to test routing

---
+++ pages/portfolio/index.js
@@ -0,0 +1,13 @@
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>
+        Portfolio Page
+      </h1>
+    </div>
+  )
+}
+
+export default PortfolioPage
+
+// to access this page : http://localhost:3000/portfolio
---
+++ pages/portfolio/list.js
@@ -0,0 +1,13 @@
+const ListPage = () => {
+  return (
+    <div>
+      <h1>
+        List Page
+      </h1>
+    </div>
+  )
+}
+
+export default ListPage
+
+// to access this page : http://localhost:3000/portfolio/list
